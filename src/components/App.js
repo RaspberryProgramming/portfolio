4b164b6ed973dc16e53cee15bacfbc061d20b1bb
Camerin <figueroa0609@protonmail.com>
2021-09-15 15:48:02 -0400

    Router changed to BrowserRouter Router was causing bug that prevented render when Link was clicked

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,24 @@
 import React from 'react';
-import { Router, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Navigation from './Navigation';
 import history from '../history';
 import Home from './Home';
+import Github from './Github';
 
-function App() {
-  return (
-    <div className="App">
-      <Router history={history}>
-        <Navigation />
-        <Route path="/" exact component={Home} />
-      </Router>
-    </div>
-  );
+const App = (props) => {
+    return (
+      <div className="App">
+        <BrowserRouter history={history}>
+          <div>
+            <Navigation />
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/github" component={Github} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </div>
+    );
 }
 
 export default App;
