b2250ebe3bd00bc0caddce5a31d3b6325e3054be
Camerin <figueroa0609@protonmail.com>
2021-09-15 15:50:36 -0400

    Component Name Fixes and stylistic fixes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './css/Home.css';
 
-function App() {
+const Home = (props) => {
   return (
     <div className="Home">
       <div className="theater">
-        <div className="theater-bg">
-        </div>
+        <div className="theater-bg" />
         <div className="theater-content">
           <h1>Welcome to my Portfolio</h1>
-          <p>This website helps you access the projects that I've worked on. You can navigate
+          <p>
+            This website helps you access the projects that I've worked on. You can navigate
             at the top to different locations in the site. Within you can find information about me,
             my github repositories, and some youtube videos I've posted. This website is coded with
             React/Redux and hosted over Vercel. You can email me at <a href="mailto:camerin@camsprojects.online">camerin@camsprojects.online</a>.
@@ -20,4 +20,4 @@ function App() {
   );
 }
 
-export default App;
+export default Home;
