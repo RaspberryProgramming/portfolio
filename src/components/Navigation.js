043943f6a194a079354785994a61ebeb22a1e1de
Camerin <figueroa0609@protonmail.com>
2021-09-15 15:46:55 -0400

    Updated Navigation to fit better Unordered List was removed to create better nav ui

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,34 +2,24 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './css/Navigation.css';
 
-function App() {
+const Navigation = (props) => {
   return (
     <div className="Navigation">
-      <ul>
-        <li>
-          <Link to="/">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link to="/about">
-            About Me
-          </Link>
-        </li>
-        <li>
-          <Link to="/github">
-            Github Repos
-          </Link>
-        </li>
-        <li>
-          <Link to="/youtube">
-            Youtube Videos
-          </Link>
-        </li>
-        
-      </ul>
+      <Link to="/">
+        Home
+      </Link>
+      <Link to="/about">
+        About Me
+      </Link>
+      <Link to="/github">
+        Github Repos
+      </Link>
+      <Link to="/youtube">
+        Youtube Videos
+      </Link>
     </div>
   );
 }
 
-export default App;
+
+export default Navigation;
