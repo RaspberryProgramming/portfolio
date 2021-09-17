644795c253a1231e0707e54e02104067026d9213
Camerin <figueroa0609@protonmail.com>
2021-09-17 18:43:19 -0400

    Bootstrap icons added to navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './css/Navigation.css';
+import { HouseDoor, FileEarmarkPerson, Github, Youtube } from 'react-bootstrap-icons';
 
 const Navigation = (props) => {
   return (
     <div className="Navigation">
       <Link to="/">
         Home
+        <HouseDoor />
       </Link>
       <Link to="/about">
         About Me
+        <FileEarmarkPerson />
       </Link>
       <Link to="/github">
         Github Repos
+        <Github />
       </Link>
       <Link to="/youtube">
         Youtube Videos
+        <Youtube />
       </Link>
     </div>
   );
