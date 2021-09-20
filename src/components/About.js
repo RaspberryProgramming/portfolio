11bc2df70acd73524da9915d3923e0359cabce6d
Camerin Figueroa <figueroa0609@protonmail.com>
2021-09-20 15:13:44 -0400

    Added about informaion and cards. Also Changed theme About now has cards, and has a dark theme Other page themes needed to be changed to account for About's theme

--- src/components/About.js
+++ src/components/About.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import './css/Github.css';
-import {getRepos} from '../actions';
+import { Link } from 'react-router-dom';
+import './css/About.css';
+import ReactImage from '../img/react.webp';
+import ProfilePic from '../img/profile.webp';
 import Theater from './Theater';
-import space from '../img/space.webp';
+import Card from './Card';
 
-class Github extends React.Component {
+
+class About extends React.Component {
+  componentDidMount () {
+    document.title = "About";
+  }
 
   render() {
     return (
       <div className="About">
-        <h1>About</h1>
-        <p>Hello World</p>
+        <Theater title="About" description="This page has information about me and about this site." extraClasses="h-50v" />
+        <Card title="Build with React" image={ReactImage}>
+          This website is created with <a href="https://reactjs.org/">React</a>, a javascript library for creating user interfaces and webapps.
+          React uses a component based design to help developers create easily re-usable code, and to help streamline the development process.
+          JSX is a part of react which makes it easy to design websites directly in javascript code.
+        </Card>
+
+        <Card title="Camerin Figueroa" image={ProfilePic}>
+          I am a computer science student at Marist College in Poughkeepsie, NY. I'm closing in on my Bachelors Degree in Computer Science.
+          I went to Orange County Community College for my Associates Degree in Computer Science. You can find a few of my projects on my
+          <a href="https://github.com/RaspberryProgramming" target="_blank" rel="noreferrer"> github</a> or look through a list in the
+          <Link to="/github"> Github Page</Link>
+        </Card>
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => {
-  return { repos: state.github.repos};
-}
-
-export default connect(mapStateToProps, { getRepos })(Github);
+export default About;
