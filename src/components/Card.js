659aff38d0b8ff22c593e5dd2be665316e2b77f4
Camerin Figueroa <figueroa0609@protonmail.com>
2021-09-20 15:12:31 -0400

    Created Card component

---
+++ src/components/Card.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './css/Card.css';
+
+const Card = (props) => {
+    console.log(props);
+    return (
+        <div className="card">
+            <div>
+                <div className="title">{props.title}</div>
+                <div className="content">
+                    {props.children}
+                </div>
+            </div>
+            <img className={props.image ? "show" : ""} src={props.image} alt={props.title}/>
+        </div>
+    );    
+};
+
+export default Card;
