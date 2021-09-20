647a222fca76e7a3953812688573838b60379190
Camerin <figueroa0609@protonmail.com>
2021-09-19 21:30:21 -0400

    Remove history.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Navigation from './Navigation';
-import history from '../history';
 import Home from './Home';
 import Github from './Github';
 
 const App = (props) => {
     return (
       <div className="App">
-        <BrowserRouter history={history}>
+        <BrowserRouter>
           <div>
             <Navigation />
             <Switch>
