5416eaae580bb91e330bf9acd98d2714a93233ce
Camerin <figueroa0609@protonmail.com>
2021-09-23 15:41:57 -0400

    Made site more mobile compatible Fixed some mobile css issues Moved Navbar to bottom on mobile

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import "./css/App.css";
 import Navigation from './Navigation';
 import Home from './Home';
 import Github from './Github';
--- src/components/Social.js
+++ src/components/Social.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './css/Social.css';
-import Card from './subcomponents/Card';
 import Theater from './subcomponents/Theater';
 
 
@@ -20,11 +18,11 @@ class Social extends React.Component {
         />
         <div className="links">
           <a href="https://github.com/RaspberryProgramming" target="_blank" rel="noreferrer">
-            <img src="/img/github.webp"/>
+            <img src="/img/github.webp" alt="Github"/>
           </a>
 
           <a href="https://www.linkedin.com/in/camerin-figueroa-2662bb157/" target="_blank" rel="noreferrer">
-            <img src="/img/linkedin.webp"/>
+            <img src="/img/linkedin.webp" alt="linked in"/>
           </a>
         </div>
       </div>
