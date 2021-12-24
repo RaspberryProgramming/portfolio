2016772b31e33e5c509843f0dfa309975a429d74
Camerin Figueroa <cam@camscode.com>
2021-12-24 06:02:19 -0500

    Updated literature in homepage

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -129,7 +129,7 @@ class Intro extends React.Component {
         <div>
             This website helps you access the projects that I've worked on. You can navigate
             at the top to different locations in the site. Within you can find information about me,
-            my github repositories, and some youtube videos I've posted. This website is coded with
+            my github repositories, and articles I've posted. This website is coded with
             React/Redux and hosted over Vercel. You can email me at <a href="mailto:cam@camscode.com">cam@camscode.com</a>.
         </div>
       </Topic>,
