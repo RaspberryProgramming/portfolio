f891010db44332562ecc3c5cf4b272ff4b09c217
Camerin Figueroa <cam@camscode.com>
2022-06-09 21:28:32 -0400

    ReactDOM is being unsupported in react 18 createRoot is now used

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import reduxThunk from 'redux-thunk';
@@ -9,10 +9,10 @@ import reducers from './reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducers, composeEnhancers(applyMiddleware(reduxThunk)));
+const container = document.getElementById('root');
 
-ReactDOM.render(
+createRoot(container).render(
   <Provider store={store}>
     <App/>
-  </Provider>,
-  document.querySelector('#root')
+  </Provider>
 );
