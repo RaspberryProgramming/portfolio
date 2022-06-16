f01b17ab3fb31f9792919dea6615f7645dfc8f46
Camerin Figueroa <cam@camscode.com>
2022-06-15 21:11:54 -0400

    Redux - Updated to use configureStore

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { applyMiddleware, compose } from 'redux';
+import { configureStore } from '@reduxjs/toolkit'
 import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, composeEnhancers(applyMiddleware(reduxThunk)));
+const store = configureStore({reducer: reducers}, composeEnhancers(applyMiddleware(reduxThunk)));
 const container = document.getElementById('root');
 
 createRoot(container).render(
